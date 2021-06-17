747b4d7d1ca7b525dc752e48caab49ebe1f376e4
hrm <hrm.2021@outlook.com>
2021-06-16 22:12:39 -0700

    update

--- public/js/post/list.js
+++ public/js/post/list.js
@@ -200,5 +200,23 @@ $(document).ready(function() {
         }
     }
 
-    
+    $('#search').keyup(function(){
+        // Declare variables
+        var input, filter, ul, li, a, i, txtValue;
+        input = document.getElementById('search');
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("categories");
+        li = ul.getElementsByTagName('li');
+      
+        // Loop through all list items, and hide those who don't match the search query
+        for (i = 0; i < li.length; i++) {
+          a = li[i].getElementsByTagName("a")[0];
+          txtValue = a.textContent || a.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+          } else {
+            li[i].style.display = "none";
+          }
+        }
+    });
 })
