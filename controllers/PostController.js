3c200589f7aebfe4ba7910a0d9306f488df99109
hrm <hrm.2021@outlook.com>
2021-07-05 01:02:06 -0700

    update

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -104,7 +104,7 @@ exports.selected_category = function(req, res) {
         } else {
             res.json({posts : posts});
         }
-    }).sort({ created_at : -1 });
+    }).sort({ rand : -1 });
 }
 
 exports.view = function(req, res) {
